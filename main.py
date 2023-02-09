7cbc8e74be1c64adffcbdb31a2a8a5ea1cabc7e7
MutaleMusona <musonamutale@gmail.com>
2023-02-09 19:40:24 +0200

    Sample Commit

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import tabula
 import json
+import os
 
-# Read the PDF file into a pandas DataFrame
-table = tabula.read_pdf("06-February-2023-Trade-Summary-Report.pdf", pages='all')
-df = table[0]
+pdf_files = ['data/03-02-2023.pdf','data/06-02-2023.pdf','data/07-02-2023.pdf','data/08-02-2023.pdf','data/09-02-2023.pdf']
+# Iterate over the list of PDF files
+for i, file_name in enumerate(pdf_files):
 
-# Convert the DataFrame to a JSON string
-json_str = df.to_json(orient="records")
+   # Extract the name of the file without the '.pdf' extension
+    json_file_name = os.path.splitext(file_name)[0] + ".json"
 
-# Save the JSON string to a file
-with open("new.json", "w") as file:
-   file.write(json_str)
+    # Extract the table from the PDF file
+    table = tabula.read_pdf(file_name, pages="all", multiple_tables=True)
+
+    # Get the table you want to extract
+    desired_table = table[0]
+    json_str = desired_table.to_json(orient='records')
+
+    # Save the table to a JSON file
+    with open(json_file_name, "w") as file:
+        file.write(json_str)
 
